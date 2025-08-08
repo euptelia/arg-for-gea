8be9cc7afedf83ace195bba7d0d1c5f57f9f2939
“euptelia” <tianlin.duan42@gmail.com>
2025-08-08 17:44:44 +0200

    corrected Slim scripts, new figures

--- bash/run_slim.sh
+++ bash/run_slim.sh
@@ -17,9 +17,33 @@
 #    slim /home/anadem/github/arg-for-gea/slim/glacial_history/contiuous_nonWF_M2b_glacialHistory_clineMap_gradualExpansion_temp2.slim
 #done
 
+#for i in {1..100} 
+#do
+#    printf "Start the %s/100 runs of M2b, cline, temp3 \n" $i
+    slim /home/anadem/github/arg-for-gea/slim/glacial_history/contiuous_nonWF_M2b_glacialHistory_clineMap_gradualExpansion_temp3.slim
+#done
+
 for i in {1..100} 
 do
-    printf "Start the %s/100 runs of M2b, cline, temp3 \n" $i
-    slim /home/anadem/github/arg-for-gea/slim/glacial_history/contiuous_nonWF_M2b_glacialHistory_clineMap_gradualExpansion_temp3.slim
+    printf "Start the %s/100 runs of M3b, cline, temp0 \n" $i
+    slim /home/anadem/github/arg-for-gea/slim/glacial_history/contiuous_nonWF_M3b_glacialHistory_clineMap_recurrentChange.slim
 done
+
+#for i in {1..100} 
+#do
+#    printf "Start the %s/100 runs of M3b, cline, temp1 \n" $i
+#    slim /home/anadem/github/arg-for-gea/slim/glacial_history/contiuous_nonWF_M3b_glacialHistory_clineMap_recurrentChange_temp1.slim
+#done
+
+#for i in {1..100} 
+#do
+#    printf "Start the %s/100 runs of M3b, cline, temp1 \n" $i
+#    slim /home/anadem/github/arg-for-gea/slim/glacial_history/contiuous_nonWF_M3b_glacialHistory_clineMap_recurrentChange_temp2.slim
+#done
+
+#for i in {1..100} 
+#do
+#    printf "Start the %s/100 runs of M3b, cline, temp3 \n" $i
+#    slim /home/anadem/github/arg-for-gea/slim/glacial_history/contiuous_nonWF_M3b_glacialHistory_clineMap_recurrentChange_temp3.slim
+#done
 
